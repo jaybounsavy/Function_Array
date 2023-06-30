6b12373f8b8de615b7405cdf9b87bdf44d899713
jaybounsavy <jay.bounsavy@gmail.com>
2023-06-30 18:16:50 -0500

    Add 4_ArrayAccessFirstElement.js

---
+++ 4_ArrayAccessFirstElement.js
@@ -0,0 +1,15 @@
+
+//****************************************************************
+// How do you access the first element of any array?
+//****************************************************************
+
+// Answer:  The first element is at index 0.
+
+// Create a new array
+let myArray = new Array( 1, 2, 3, 4, 5, 6 );
+
+// Access the last element
+let firstElement = myArray[0];
+
+// Display the last element
+console.log( "The first element = " + firstElement ); 
