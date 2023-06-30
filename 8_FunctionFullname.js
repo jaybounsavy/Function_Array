01b45aadaf82660cb7018db7bb763b331df6d3e4
jaybounsavy <jay.bounsavy@gmail.com>
2023-06-30 18:47:43 -0500

    Add 8_FunctionFullname.js

---
+++ 8_FunctionFullname.js
@@ -0,0 +1,17 @@
+
+//************************************************************************************************************************
+// 8. Write a function that takes two parameters, firstName and lastName, 
+//    and returns a full name. The full name should be the first and the last name separated by a space.
+//************************************************************************************************************************
+
+
+// Function to return a fullname
+function fullName( firstName, lastName ) {
+
+    // Return full name
+    return firstName + " " + lastName;
+    
+}
+
+// Call the function and display the result
+console.log( "Fullname = " + fullName( "Tom", "Cruise") );
