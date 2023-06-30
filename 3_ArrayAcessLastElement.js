552683f99d11c71523f1e5e1e4ae9998e90965c0
jaybounsavy <jay.bounsavy@gmail.com>
2023-06-30 18:11:35 -0500

    Add 3_ArrayAcessLastElement.js

---
+++ 3_ArrayAcessLastElement.js
@@ -0,0 +1,15 @@
+
+//****************************************************************
+// How do you access the last element of any array?
+//****************************************************************
+
+// Answer:  Use [.length - 1]
+
+// Create a new array
+let myArray = new Array( 1, 2, 3, 4, 5, 6 );
+
+// Access the last element
+let lastElement = myArray[myArray.length-1];
+
+// Display the last element
+console.log( "The last element = " + lastElement ); 
